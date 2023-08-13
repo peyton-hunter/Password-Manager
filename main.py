f51c321adc80a6983102dd0adb01ac7a14aca7bd
paintinghunter <peytonhunter.cooper@gmail.com>
2023-08-13 16:34:32 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,93 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+import json
+
+
+def generate():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+               'i', 'j,', 'k', 'l', 'm', 'n', 'o', 'p',
+               'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+               'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+               ]
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '*', '(', ')', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_char = password_letters + password_numbers + password_symbols
+    shuffle(password_char)
+    password = "".join(password_char)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
+
+
+def save():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops...", message="Oops! It looks like you left out some information.")
+    else:
+        save_data = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {email}"
+                                                                  f"\nPassword: {password} \nIs it okay to save?")
+        if save_data:
+            try:
+                with open("data.json", "r") as file:
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open("data.json", "w") as file:
+                    json.dump(data, file, indent=4)
+            else:
+                data.update(new_data)
+                with open("data.json", "w") as file:
+                    json.dump(data, file, indent=4)
+            finally:
+                website_input.delete(0, END)
+                password_input.delete(0, END)
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(height=200, width=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+website_input = Entry(width=38)
+website_input.focus()
+website_input.grid(row=1, column=1, columnspan=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+email_input = Entry(width=38)
+email_input.grid(row=2, column=1, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+password_input = Entry(width=21)
+password_input.grid(row=3, column=1)
+
+generate_password = Button(text="Generate Password", command=generate)
+generate_password.grid(row=3, column=2)
+
+add = Button(text="Add", width=36, command=save)
+add.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
